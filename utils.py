4f219e431c25ea3cbce6a1fd8cee4be38dfac629
np-csu <nipan1988@gmail.com>
2017-11-13 22:05:03 +0800

    Updata resize_image function
    when run with python2, it will cause TypeError: integer argument expected, got float

--- utils.py
+++ utils.py
@@ -392,7 +392,7 @@ def resize_image(image, min_dim=None, max_dim=None, padding=False):
     # Resize image and mask
     if scale != 1:
         image = scipy.misc.imresize(
-            image, (round(h * scale), round(w * scale)))
+            image, (int(h * scale), int(w * scale)))
     # Need padding?
     if padding:
         # Get new height and width
